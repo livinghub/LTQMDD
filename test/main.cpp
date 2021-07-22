db0784c0cfbd60b355abac52cb1a9a4bc036d578
lyh <707884308@qq.com>
2021-07-22 15:57:37 +0800

    lt测试

--- test/main.cpp
+++ test/main.cpp
@@ -31,6 +31,7 @@ int main(int argc, char *argv[]) {
 	std::cout << dd->size(functionality) << std::endl; //输出原始dd的大小
 
 	qc::permutationMap varMap = qc.initialLayout; //复制电路的初始变量映射
+	qc::permutationMap ltvarMap = varMap;
 	qc.printPermutationMap(varMap); //打印初始映射
 
 	// 调用重排算法
@@ -40,9 +41,15 @@ int main(int argc, char *argv[]) {
 	std::cout << dd->size(reorderdd) << std::endl; //输出重排后dd的大小
 	qc.printPermutationMap(varMap); //输出重排后的变量映射关系
 
-	//判断重排前后dd是否一致
-	std::cout << "重排电路是否一致： " << dd->equals(functionality, reorderdd) << std::endl;
+	// 调用重排算法
+	auto ltreod = dd->dynamicReorder(functionality, ltvarMap, dd::DynamicReorderingStrategy::linearSift);
+	//qc.printMatrix(dd, reorderdd, std::cout); //输出矩阵
+	dd::export2Dot(ltreod, "ltreod.dot"); //输出QMDD表示文件
+	std::cout << dd->size(ltreod) << std::endl; //输出重排后dd的大小
+	qc.printPermutationMap(ltvarMap); //输出重排后的变量映射关系
 
+	//判断重排前后dd是否一致
+	//std::cout << "重排电路是否一致： " << dd->equals(functionality, reorderdd) << std::endl;
 
 	return 0;
 }
